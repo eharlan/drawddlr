564336903daf927767bb70f2b9b7218caf93db2a
Erik Harlan <harlanes14@gmail.com>
2023-02-04 17:12:26 -0700

    Basic stars example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
-import logo from './logo.png';
+import React from 'react';
+import { Stage, Layer, Star, Text, Circle } from 'react-konva';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h2>Drawddlr</h2>
-          <p>A Drawing app for kids</p>
-      </header>
-    </div>
-  );
+const generateShapes = () => {
+  return [...Array(17)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+const INITIAL_STATE = generateShapes();
+const randomColor = () => "#000000".replace(/0/g,() => (~~(Math.random()*16)).toString(16));
+
+
+const App = () => {  
+  const [stars, setStars] = React.useState(INITIAL_STATE);
+
+  const handleDragStart = (e) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+
+  const handleDragEnd = (e) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+  
+
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {stars.map((star) => (
+            <Star
+              key={star.id}
+              id={star.id}
+              x={star.x}
+              y={star.y}
+              numPoints={5}
+              innerRadius={100}
+              outerRadius={50}
+              fill={randomColor()}
+              opacity={0.8}
+              draggable
+              rotation={star.rotation}
+              shadowColor="black"
+              shadowBlur={10}
+              shadowOpacity={0.6}
+              shadowOffsetX={star.isDragging ? 10 : 5}
+              shadowOffsetY={star.isDragging ? 10 : 5}
+              scaleX={star.isDragging ? 1.8 : 1}
+              scaleY={star.isDragging ? 1.8 : 1}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            />
+          ))}
+        </Layer>
+      </Stage>
+    );
 }
 
 export default App;
